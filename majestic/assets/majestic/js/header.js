59c3f843cfd13072d100362ee1f0d6fc277fc7e2
Denis Vyzulin <denis-vyzulin@mail.ru>
2023-02-05 14:07:22 +0300

    feat: Create header sidebar with adaptability

---
+++ majestic/assets/majestic/js/header.js
@@ -0,0 +1,23 @@
+let burger = document.getElementById('burger'),
+    header = document.getElementById('header'),
+    body = document.getElementById('body');
+
+burger.addEventListener('click', () => {
+    if (header.classList.contains('_active')) {
+        header.classList.remove('_active');
+        burger.classList.remove('_active');
+        body.classList.remove('_active');
+    } else {
+        header.classList.add('_active');
+        burger.classList.add('_active');
+        body.classList.add('_active');
+
+        header.addEventListener('click', el => {
+            if (el.target == header) {
+                header.classList.remove('_active');
+                burger.classList.remove('_active');
+                body.classList.remove('_active');
+            }
+        });
+    }
+});
