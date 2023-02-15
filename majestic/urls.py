4f947816b78d8459e7105aa94239e36a9d12ff77
Denis Vyzulin <denis-vyzulin@mail.ru>
2023-02-15 06:43:50 +0300

    feat: create all pages of services

--- majestic/urls.py
+++ majestic/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = 'majestic'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('services/', views.services, name='services'),
+    path('services/vyshivka-na-kroe', views.vnkroe, name='vnkroe'),
+    path('services/shevroni-i-nashivki', views.shevnash, name='shevnash'),
+    path('services/vyshivka-na-gotovyh-izdeliyah', views.vngotizd, name='vngotizd'),
+    path('services/cerkovnaya-vishivka', views.cerkvysh, name='cerkvysh'),
     path('price/', views.price, name='price'),
 ]
--- majestic/views.py
+++ majestic/views.py
@@ -5,5 +5,21 @@ def home(request):
     return render(request, 'majestic/index.html')
 
 
+def services(request):
+    return render(request, 'majestic/services.html')
+
+def vnkroe(request):
+    return render(request, 'majestic/services/vnkroe.html')
+
+def shevnash(request):
+    return render(request, 'majestic/services/shevnash.html')
+
+def vngotizd(request):
+    return render(request, 'majestic/services/vngotizd.html')
+
+def cerkvysh(request):
+    return render(request, 'majestic/services/cerkvysh.html')
+
+
 def price(request):
     return render(request, 'majestic/price.html')
